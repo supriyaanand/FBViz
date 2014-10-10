754cc4f6ff92b433d9a8b73a07a4a81ed5125471
supriyaanandrao <supriya.anandrao@gmail.com>
2014-10-10 13:22:17 +0530

    Data collection added

--- FBViz.py
+++ FBViz.py
@@ -2,7 +2,7 @@ import json
 import requests
 from pprint import pprint
 
-access_token = 'CAACEdEose0cBANSRdUQQs2qAICytY2rc4EYnK79AvTjX82PI2gLZC1eVR0kMIppGaKE5GjKSTGOFceHzoCYNBf3j1VjstgfeCmGJ85ZBFogsPfeSZB5CtALInYaZBxtQNsOTp5YcjYKTh72ZC9boBg2bgwd2rdaNPLrsWzTpShmn99J3UEl5uscTxFBynwWP0c3XG0t9OZChSaGReVNA3nZBgZAjgj2UFgMZD'
+access_token = 'CAACEdEose0cBAG5ceGyZCaoNJYqdrGi64LmVv1rlmRfkqrOHkwJocZActqnZBzapLzYEuaKJ6Ein5Gxaes2UlkAFkgXIV0iv2NZC7MSp3rwiLB0ZC3LE4rARQVZA0zDor3kJvycQ6FHAIWStpAlJRWwltghblARqG4UDsPuvdcS5bGlffHCiOpJNc6GLN44IxyqX0XTZAoKZBFzPVZBuYRowNiKLTjw3P9nEZD'
 
 access_str = '&access_token=' + access_token
 # r = requests.get('https://graph.facebook.com/me/friends?fields=name,id,likes.fields(id,name,category)'+access_str)
@@ -37,7 +37,7 @@ friends_books = {}
 friends_sports = {}
 friends_movies = {}
 
-url = 'https://graph.facebook.com/me/friends?fields=name,likes.fields(name,category),music.fields(name,category),movies.fields(name,category)' + access_str
+url = 'https://graph.facebook.com/me/friends?fields=name,likes.fields(name,category),music.fields(name,category),movies.fields(name,category),books.fields(name,category),sports' + access_str
 
 r = requests.get(url)
 raw_data = []
@@ -46,7 +46,6 @@ count = 0
 while True:
     r = requests.get(url)
     raw_data.insert(count, r.json())
-    pprint(raw_data[count])
     try:
         next_url = raw_data[count]['paging']['next'].encode('utf-8')
     except:
@@ -54,24 +53,58 @@ while True:
     count += 1
     url = next_url
 
-pprint(raw_data)
 
-for dat in raw_data[0]['data']:
-    pprint(dat)
-    likes_list = []
-    likes_cat_list = []
-    name = dat['name']
-    try:
-        for like in dat['likes']['data']:
-            likes_list.append(like['name'])
-            likes_cat_list.append(like['category'])
-        print name
-        print likes_list
-    except:
-        pass
-'''
-        for like in dat['data']:
-                print 'id ' + like['id']
-                friends_likes.append(like['name'])
-                friends_likes_cat.append(like['category'])
-'''
+for info in raw_data:
+    for dat in info['data']:
+        likes_list = []
+        likes_cat_list = []
+        music_list = []
+        movie_list = []
+        books_list = []
+        sport_list = []
+        name = dat['name']
+        try:
+            for like in dat['likes']['data']:
+                likes_list.append(like['name'])
+                likes_cat_list.append(like['category'])
+            friends_likes[name] = likes_list
+        except:
+            pass
+        try:
+            for music in dat['music']['data']:
+                if music['category'] in ('Musician/band','Music'):
+                    music_list.append(music['name'])
+            friends_music[name] = music_list
+        except:
+            pass
+        try:
+            for movie in dat['movies']['data']:
+                movie_list.append(movie['name'])
+            friends_movies[name] = movie_list
+        except:
+            pass
+        try:
+            for book in dat['books']['data']:
+                books_list.append(book['name'])
+            friends_books[name] = books_list
+        except:
+            pass
+        try:
+            for sport in dat['sports']:
+                sport_list.append(sport['name'])
+            friends_sports[name] = sport_list
+        except:
+            pass
+
+print "likes\n"
+print friends_likes
+print 'movies\n'
+print friends_movies
+print 'music\n'
+print friends_music
+print 'books\n'
+print friends_books
+print 'sports\n'
+print friends_sports
+
+
